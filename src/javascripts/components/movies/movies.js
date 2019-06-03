e865be53dda18e7245e86f13c972f6f829168cdb
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-06-03 17:32:31 -0600

    wip

--- src/javascripts/components/movies/movies.js
+++ src/javascripts/components/movies/movies.js
@@ -1,18 +1,11 @@
-import movieData from '../../helpers/data/moviesData';
+// import movieData from '../../helpers/data/moviesData';
 import util from '../../helpers/util';
 import 'bootstrap';
 
-const movieDomStringBuilder = (uid) => {
-  movieData.getMovieByUid(uid).then((movies) => {
-    let domString = '';
-    movies.forEach((movie) => {
-      domString += `<h1>${movie.title}</h1>`;
-      domString += `<img src=${movie.imageUrl} />`;
-      domString += `<h2>${movie.rating}</h2>`;
-      domString += '<button class ="btn btn-info">Add Rating</button>';
-    });
-    util.printToDom('movie', domString);
-  }).catch(err => console.error('could not get movie', err));
+const movieDomStringBuilder = () => {
+  let domstring = '';
+  domstring += '<h2>MOVIES</h2>';
+  util.printToDom('movies', domstring);
 };
 
 export default { movieDomStringBuilder };
